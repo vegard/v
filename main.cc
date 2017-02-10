f8694a16c167b921f8de7314dece91a11a03a4be
Vegard Nossum <vegard.nossum@gmail.com>
2017-02-10 20:54:07 +0100

    main: create wrappers for reading/parsing files

--- main.cc
+++ main.cc
@@ -10,6 +10,7 @@ extern "C" {
 }
 
 #include <functional>
+#include <memory>
 
 #include "ast.hh"
 #include "line_number_info.hh"
@@ -47,50 +48,109 @@ static void eval(const ast_node_ptr node)
 	}
 }
 
-int main(int argc, char *argv[])
-{
-	for (int i = 1; i < argc; ++i) {
-		const char *filename = argv[i];
+struct document {
+	const char *name;
+
+	const char *data;
+	size_t data_size;
+
+	std::unique_ptr<line_number_info> _line_numbers;
+
+	document()
+	{
+	}
+
+	document(const char *name, const char *data, size_t data_size):
+		name(name),
+		data(data),
+		data_size(data_size)
+	{
+	}
+
+	virtual ~document()
+	{
+	}
+
+	line_number_info &line_numbers()
+	{
+		// Load line number data lazily
+		if (!_line_numbers)
+			_line_numbers = std::make_unique<line_number_info>(data, data_size);
+
+		return *_line_numbers;
+	}
+
+	ast_node_ptr parse()
+	{
+		unsigned int pos = 0;
+		return parser(data, data_size).parse_doc(pos);
+	}
+};
+
+struct file_document: document {
+	const char *filename;
+	struct stat stbuf;
+	void *mem;
+
+	file_document(const char *filename):
+		filename(filename),
+		mem(nullptr)
+	{
+		// TODO: throw exceptions instead of exiting
 
 		int fd = open(filename, O_RDONLY);
 		if (fd == -1)
 			error(EXIT_FAILURE, errno, "%s: open()", filename);
 
-		struct stat stbuf;
 		if (fstat(fd, &stbuf) == -1)
 			error(EXIT_FAILURE, errno, "%s: fstat()", filename);
 
-		void *mem = mmap(nullptr, stbuf.st_size, PROT_READ, MAP_SHARED, fd, 0);
+		mem = mmap(nullptr, stbuf.st_size, PROT_READ, MAP_SHARED, fd, 0);
 		if (mem == MAP_FAILED)
 			error(EXIT_FAILURE, errno, "%s: mmap()", filename);
 
 		close(fd);
 
-		const char *doc = (const char *) mem;
-		size_t doc_size = stbuf.st_size;
+		// initialize parent
+		name = filename;
+		data = (const char *) mem;
+		data_size = stbuf.st_size;
+	}
 
-		parser p(doc, doc_size);
+	~file_document()
+	{
+		munmap(mem, stbuf.st_size);
+	}
+};
+
+static void print_message(document &doc, unsigned int pos_byte, unsigned int end_byte, std::string message)
+{
+	line_number_info &line_numbers = doc.line_numbers();
+	auto pos = line_numbers.lookup(pos_byte);
+	auto end = line_numbers.lookup(end_byte);
+
+	printf("%s:%u:%u: %s\n", doc.name, pos.line, pos.column, message.c_str());
+	if (pos.line == end.line) {
+		printf("%.*s", pos.line_length, doc.data + pos.line_start);
+		printf("%*s%s\n", pos.column, "", std::string(end.column - pos.column, '^').c_str());
+	} else {
+		// TODO: print following lines as well?
+		printf("%.*s", pos.line_length, doc.data + pos.line_start);
+		assert(pos.line_length - 1 >= pos.column + 1);
+		printf("%*s%s\n", pos.column, "", std::string(pos.line_length - 1 - pos.column, '^').c_str());
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	for (int i = 1; i < argc; ++i) {
+		file_document doc(argv[i]);
 		ast_node_ptr node;
 
 		try {
-			unsigned int pos = 0;
-			node = p.parse_doc(pos);
+			node = doc.parse();
 		} catch (const parse_error &e) {
-			line_number_info line_numbers(doc, doc_size);
-			auto pos = line_numbers.lookup(e.pos);
-			auto end = line_numbers.lookup(e.end);
-
-			printf("%s:%u:%u: %s\n", filename, pos.line, pos.column, e.what());
-			if (pos.line == end.line) {
-				printf("%.*s", pos.line_length, doc + pos.line_start);
-				printf("%*s%s\n", pos.column, "", std::string(end.column - pos.column, '^').c_str());
-			} else {
-				// TODO: print following lines as well?
-				printf("%.*s", pos.line_length, doc + pos.line_start);
-				assert(pos.line_length - 1 >= pos.column + 1);
-				printf("%*s%s\n", pos.column, "", std::string(pos.line_length - 1 - pos.column, '^').c_str());
-			}
-
+			print_message(doc, e.pos, e.end, e.what());
 			return EXIT_FAILURE;
 		}
 
@@ -103,8 +163,6 @@ int main(int argc, char *argv[])
 			for (ast_node_ptr stmt: traverse<AST_SEMICOLON>(node))
 				eval(stmt);
 		}
-
-		munmap(mem, stbuf.st_size);
 	}
 
 	return EXIT_SUCCESS;

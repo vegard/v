f1a7ba7a444ed0bf782a5bfc4b146541bcf25ba2
Vegard Nossum <vegard.nossum@gmail.com>
2017-07-23 09:06:53 +0200

    builtin/while: add test

--- test.sh
+++ test.sh
@@ -7,3 +7,8 @@ for file in tests/parser/*.v
 do
 	diff -U100 ${file%.v}.out <(./v --dump-ast --no-compile $file)
 done
+
+for file in tests/builtin/*.v
+do
+	diff -U100 ${file%.v}.out <(./v $file)
+done

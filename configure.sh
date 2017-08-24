a0e9210caa0e474ba0927a3758739bebc4c3215e
Vegard Nossum <vegard.nossum@gmail.com>
2017-08-24 23:10:49 +0200

    configure.sh: setup udis86 properly
    This makes ./configure.sh work properly on a freshly cloned repository.

--- configure.sh
+++ configure.sh
@@ -3,9 +3,13 @@
 set -e
 set -u
 
+git submodule init
+git submodule update
+
 udis86_prefix=$PWD/udis86-install
 
 (
 	cd udis86
+	./autogen.sh
 	./configure --enable-static=yes --enable-shared=no --prefix="${udis86_prefix}"
 )

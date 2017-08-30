f1a2ca6ca99f634a0a1c2aca9b0126dd07225c65
Vegard Nossum <vegard.nossum@gmail.com>
2017-08-30 19:46:28 +0200

    test.sh: print test filenames

--- test.sh
+++ test.sh
@@ -5,10 +5,12 @@ set -u
 
 for file in tests/parser/*.v
 do
-	diff -U100 ${file%.v}.out <(./v --dump-ast --no-compile $file)
+	echo $file
+	diff -U100 ${file%.v}.out <(./v --dump-ast --no-compile $file) || true
 done
 
 for file in tests/builtin/*.v
 do
-	diff -U100 ${file%.v}.out <(./v $file)
+	echo $file
+	diff -U100 ${file%.v}.out <(./v $file) || true
 done

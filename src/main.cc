71042871b93af02bcb4928bbceed3a4c970fddd7
Vegard Nossum <vegard.nossum@gmail.com>
2017-11-01 22:30:39 +0100

    main: remove --dump-binary
    This was only really useful as a debugging tool before getting udis86 so
    I could look at the generated assembly code.
    
    Let's simplify by getting rid of this now. We need a new mechanism anyway
    for actually producing binary files.

--- src/main.cc
+++ src/main.cc
@@ -153,7 +153,6 @@ int main(int argc, char *argv[])
 {
 	bool do_dump_ast = false;
 	bool do_compile = true;
-	bool do_dump_binary = false;
 	bool do_run = true;
 	std::vector<const char *> filenames;
 
@@ -163,8 +162,6 @@ int main(int argc, char *argv[])
 				do_dump_ast = true;
 			else if (!strcmp(argv[i], "--no-compile"))
 				do_compile = false;
-			else if (!strcmp(argv[i], "--dump-binary"))
-				do_dump_binary = true;
 			else if (!strcmp(argv[i], "--no-run"))
 				do_run = false;
 			else if (!strcmp(argv[i], "--disassemble"))
@@ -202,17 +199,6 @@ int main(int argc, char *argv[])
 			} catch (const compile_error &e) {
 				print_message(doc, e.pos, e.end, e.what());
 				return EXIT_FAILURE;
-			}
-
-			if (do_dump_binary) {
-				std::string output_filename = std::string(filename) + ".bin";
-
-				FILE *fp = fopen(output_filename.c_str(), "wb+");
-				if (!fp)
-					error(EXIT_FAILURE, errno, "%s: fopen()", output_filename.c_str());
-				if (fwrite(&f->bytes[0], f->bytes.size(), 1, fp) != 1)
-					error(EXIT_FAILURE, errno, "%s: fwrite()", output_filename.c_str());
-				fclose(fp);
 			}
 
 			if (do_run)
